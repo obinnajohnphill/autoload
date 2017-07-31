1111eeeecc39157b02cf348123b3f1088142784d
Obinna Johnphill <obinna.johnphill@googlemail.com>
2017-07-31 23:56:36 +0100

    update

--- index.php
+++ index.php
@@ -2,12 +2,13 @@
 require "vendor/autoload.php";
 
 use Obinna\User;
-use Obinna\Page;
+use Obinna\Page\Page;
 
 
+$user = new User();
 $page = new Page();
 
-$helloUser = $page->sayhello();
+$helloUser = $user->sayhello();
 $hellopage = $page->another();
 
 echo $helloUser."\n\n".$hellopage;
