fbd460e9df2fc21748ae7d20bc8f1fb3c923c339
Obinna Johnphill <obinna.johnphill@googlemail.com>
2017-07-31 22:48:57 +0100

    first commit

---
+++ src/Page.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Acme\Pages;
+
+class Page {
+    function another()
+    {
+        echo "hello,this is another page.";
+    }
+}
---
+++ src/User.php
@@ -0,0 +1,9 @@
+<?php
+namespace Obinna;
+
+class Page {
+    public function sayhello()
+    {
+        echo "hello, this is a user page.";
+    }
+}
