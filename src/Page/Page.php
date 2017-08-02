6259c640c3f1bccd4152f2d28d9e6dfa39554a88
Obinna Johnphill <obinna.johnphill@enta.net>
2017-08-02 10:38:03 +0100

    final update

--- src/Page/Page.php
+++ src/Page/Page.php
@@ -5,6 +5,6 @@ namespace Obinna\Page;
 class Page {
     function another()
     {
-        echo "hello again,this is another page.";
+        echo "hello again,this text comes from another class in src/Page/Page.php";
     }
 }
