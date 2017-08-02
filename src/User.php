a35fefb9e85312f35fabd5b4f426899c6bc68439
Obinna Johnphill <obinna.johnphill@enta.net>
2017-08-02 10:37:23 +0100

    final update

--- src/User.php
+++ src/User.php
@@ -1,9 +1,10 @@
 <?php
+
 namespace Obinna;
 
 class User {
     public function sayhello()
     {
-        echo "hello world, this is a user page.<br><br>";
+        echo "hello world, this text comes from a user class in src/User.php<br><br>";
     }
 }
